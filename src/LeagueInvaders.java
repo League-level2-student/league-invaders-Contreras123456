b41b1f72fa1ad4ae7079a46fd026b11f2f601c93
Contreras123456 <35355212+Contreras123456@users.noreply.github.com>
2020-07-18 12:59:34 -0700

    :+1:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	JPanel panel = new JPanel();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	LeagueInvaders(){
+		frame = new JFrame();
+	}	
+public static void main(String[] args) {
+	
 }
+void setup() {
+	frame.setSize(WIDTH,HEIGHT);
+	frame.setVisible(true);
+	frame.add(panel);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+}
+
