c73736f8e5545f80fced191c89f18265d3dc1a9a
Contreras123456 <35355212+Contreras123456@users.noreply.github.com>
2020-10-10 12:58:44 -0700

    :+1:

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,10 @@
 
 public class ObjectManager {
-
+Rocketship rocket;
+public static void main(String[] args) {
+	
+}
+ObjectManager(Rocketship rocket){
+rocket = new Rocketship(250, 700, 50, 50);
+}
 }
