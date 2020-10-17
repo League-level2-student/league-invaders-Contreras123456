accaa2dc5b3b2765de8e92c08aa6e231376b9657
Contreras123456 <35355212+Contreras123456@users.noreply.github.com>
2020-10-17 12:56:45 -0700

    :+1:

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 Rocketship rocket;
+ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+ArrayList<Alien> aliens = new ArrayList<Alien>();
+Random random = new Random();
 public static void main(String[] args) {
 	
 }
-ObjectManager(Rocketship rocket){
-rocket = new Rocketship(250, 700, 50, 50);
+ObjectManager(Rocketship rocketship){
+rocket = rocketship;
+}
+void addAlien(){
+	aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+}
+void update() {
+	
 }
 }
