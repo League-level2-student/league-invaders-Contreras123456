e55bac599b2022ed1a3e1b1d2346e5646f766ce7
Contreras123456 <35355212+Contreras123456@users.noreply.github.com>
2020-10-24 12:59:45 -0700

    :+1:

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -16,6 +17,27 @@ void addAlien(){
 	aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
 }
 void update() {
-	
+for (int i = 0; i < aliens.size(); i++) {
+	aliens.get(i).update();
+	if (aliens.get(i).y> LeagueInvaders.HEIGHT) {
+		aliens.get(i).isActive = false;
+	}
+}	
+for (int i = 0; i < projectiles.size(); i++) {
+	projectiles.get(i).update();
+	if (projectiles.get(i).y> LeagueInvaders.HEIGHT) {
+		projectiles.get(i).isActive = false;
+	}
+}
+
+}
+void draw(Graphics g) {
+	rocket.draw(g);
+	for (int i = 0; i < aliens.size(); i++) {
+		aliens.get(i).draw(g);
+	}
+	for (int i = 0; i < projectiles.size(); i++) {
+		projectiles.get(i).draw(g);
+	}
 }
 }
